258ff69d97ffc9b3ea481a7499f3e6ee3c97bc09
Your Name <you@example.com>
2020-05-05 19:35:20 +0300

    commit

---
+++ src/lesson1/Fruit.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public abstract class Fruit {
+    protected float weight;
+
+    public abstract Fruit newInstance();
+
+    public float getWeight() {
+
+        return weight;
+    }
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/lesson1/Orange.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class Orange extends Fruit {
+    public Orange() {
+
+        super(1.5f);
+    }
+
+    @Override
+    public Fruit newInstance() {
+        return new Orange();
+    }
+}
